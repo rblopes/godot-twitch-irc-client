df8b23f6dbeee83ee08adb6630648fef462045f7
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2023-04-02 18:31:44 -0300

    fix user list displaying duplicate entries

--- demo/users/users.gd
+++ demo/users/users.gd
@@ -1,16 +1,20 @@
 extends VBoxContainer
 
 
+func _find_index(username: String) -> int:
+	for i in $List.item_count:
+		if $List.get_item_text(i) == username:
+			return i
+	return -1
+
+
 func add_user(username: String) -> void:
-	$List.add_item(username)
-	$List.sort_items_by_text()
+	if _find_index(username) < 0:
+		$List.add_item(username)
+		$List.sort_items_by_text()
 
 
 func remove_user(username: String) -> void:
-	var index := -1
-	for i in $List.item_count:
-		if $List.get_item_text(i) == username:
-			index = i
-			break
+	var index := _find_index(username)
 	if index >= 0:
 		$List.remove_item(index)
